b0abfad9438048f068be6cf58d26595a6b0c89a7
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-01-26 18:53:29 +0530

    Initial commit of Spring Boot app

---
+++ src/main/java/com/lifecare/lifecare/LifecareApplication.java
@@ -0,0 +1,13 @@
+package com.lifecare.lifecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifecare/lifecare/LifecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifecare.lifecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
